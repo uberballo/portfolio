517df52ca0965d96d716f6b26ed5d5ebfc853bfa
uberballo <kasper.paavolainen@hotmail.com>
2020-02-07 17:14:16 +0200

    gh action fix4

--- backend/controllers/codeController.js
+++ backend/controllers/codeController.js
@@ -4,7 +4,7 @@ const CreateCode = async(req, res) =>{
     try{
         console.log(req.body)
         const code = await models.Code.create(req.body)
-        return res.status(200).json({
+        return res.status(201).json({
             code
         })
     } catch (error){
@@ -15,7 +15,7 @@ const CreateCode = async(req, res) =>{
 const GetAllCodes = async(req, res) =>{
     try{
        const codes = await models.Code.findAll()
-       return res.status(201).json({
+       return res.status(200).json({
            codes
        })
     } catch (error) {
