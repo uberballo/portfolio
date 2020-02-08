4f2e36107ca7b7ff3df41b8fde153ea9e5595343
uberballo <kasper.paavolainen@hotmail.com>
2020-02-08 16:09:37 +0200

    remove unused function

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,6 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static('frontend/build'))
 }
 
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-});
 
 app.use('/api', routes)
 
