1d6e2d00ae6ca2190dea77905eab434693f5cb0d
uberballo <kasper.paavolainen@hotmail.com>
2020-02-08 14:51:58 +0200

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api', routes)
