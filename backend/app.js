a9b1679687bf3a863afdd338a7e4be603cd5d76c
uberballo <kasper.paavolainen@hotmail.com>
2020-02-08 15:50:46 +0200

    heroku fix 1

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
 app.use('/api', routes)
 
 module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 
 const server = http.createServer(app)
