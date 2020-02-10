a3058c6b7f06bd3606e0d9b195584ff1845b9d08
uberballo <kasper.paavolainen@hotmail.com>
2020-02-10 15:54:28 +0200

    First E2E test

--- frontend/cypress/integration/home_page_spec.js
+++ frontend/cypress/integration/home_page_spec.js
@@ -1,6 +1,16 @@
 describe('The hope page', function(){
+    
     it('successfully loads', function(){
         cy.visit('/')
-    }
-    )
+        cy.visit('/about')
+    }),
+
+    it('Project is found and can be chosen', function(){
+        cy.visit('/')
+        cy.contains('div','java peli').parent().within(($div) =>{
+            cy.get('button').click()
+        })
+        cy.contains('Java koodia')
+     })
+    
 })
