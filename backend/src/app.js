3f7f8e3fc642a00c9c83c514dadb381b6ca51b0f
uberballo <kasper.paavolainen@hotmail.com>
2020-02-12 16:08:21 +0200

    refactoring structure

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: process.env.SENTRY_DNS });
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+}
+
+app.use('/api', routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/'));
+});
+module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import codeService from './sevices/codeService';
 import HomePageContainer from './components/container/HomePageContainer';
 import './App.css';
 
